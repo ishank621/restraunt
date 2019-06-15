89459e6ac15bd0da4295599378093c44dbe17942
Ishank <ishank@classplus.co>
2019-06-15 19:19:21 +0530

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,21 +12,33 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+        dishes: DISHES
+        // selectedDish: null
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
+  // onDishSelect(dishId) {
+  //   this.setState({ selectedDish: dishId});
+  // }
+  
   render() {
+
+    const HomePage = () => {
+      return(
+        <Home />
+      );
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+          <Route path="/home"component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to = "/home" />
+        </Switch>
+        {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
+
         <Footer />
       </div>
     );
