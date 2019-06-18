a66ef7f2bb975a98efad8f4dbc0180e7768de4ce
Ishank <ishank@classplus.co>
2019-06-18 17:48:26 +0530

    Redux Form Validation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import Main from "./components/MainComponent";
-import "./App.css";
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 import { Provider } from 'react-redux';
-import { ConfigureStore } from './Redux/configureStore';
-
+import { ConfigureStore } from './redux/configureStore';
 
 const store = ConfigureStore();
 
 class App extends Component {
-
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <div className="App">
+          <div>
             <Main />
           </div>
         </BrowserRouter>
