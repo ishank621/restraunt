89c5fbc8341a37e4d11292252851b4a6360e40ac
Ishank <ishank@classplus.co>
2019-06-18 18:30:02 +0530

    Removing reducer

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -83,8 +83,8 @@ export default class Header extends Component {
                     <div className="container">
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
-                                <h1>Ristorante Con Fusion</h1>
-                                <p>We take inspiration from the world's jlsfjldsjfljldsjllj</p>
+                                <h1>Ishank Restaurant</h1>
+                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                             </div>
                         </div>
                     </div>
