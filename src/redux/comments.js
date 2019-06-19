eb9f268fdc14d84dfff272fbdbd15d5b9c6d9bf7
Ishank <ishank@classplus.co>
2019-06-19 21:52:14 +0530

    Redux Thunk & Logger

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,15 +1,15 @@
 import { COMMENTS } from '../shared/comments';
-import * as ActionTypes from './ActionTypes';
-
+import * as ActionTypes from './actionTypes';
 
 export const Comments = (state = COMMENTS, action) => {
-    switch(action.type){
-        case ActionTypes.ADD_COMMENT:
+    switch (action.type) {
+        case ActionTypes.ADD_COMMENT: 
             var comment = action.payload;
-            comment.id = state.length //comments is an array of comments and .length gives the id next to the last 1 since array starts from 0
+            comment.id = state.length;
             comment.date = new Date().toISOString();
-            return state.concat(comment); //pushes a new elemen t into the array which the state has, thus, not modifing the state coz we cant directly mutate the state
-            default:
+            return state.concat(comment);
+
+        default:
             return state;
     }
 }
