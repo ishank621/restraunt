04a8e155a3b063bbe886acad1ad5bdf524664467
Ishank <ishank@classplus.co>
2019-06-22 15:24:02 +0530

    fetch from json-server

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,6 +1,7 @@
 import * as ActionTypes from './actionTypes';
 import { DISHES } from '../shared/dishes';
 import { actionTypes } from 'react-redux-form';
+import { baseUrl } from '../shared/baseUrl';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -15,9 +16,12 @@ export const addComment = (dishId, rating, author, comment) => ({
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
 
-    setTimeout(() => { 
-        dispatch(addDishes(DISHES));
-     }, 2000);
+    return fetch(baseUrl + 'dishes')
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)));
+    // setTimeout(() => { 
+    //     dispatch(addDishes(DISHES));
+    //  }, 2000);
 }
 
 export const dishesLoading = ()  => (dispatch) => ({
@@ -32,4 +36,50 @@ export const dishesFailed = (errmess) => ({
 export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
+});
+
+export const fetchComments = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+
+    return fetch(baseUrl + 'comments')
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)));
+    // setTimeout(() => { 
+    //     dispatch(addDishes(DISHES));
+    //  }, 2000);
+}
+
+export const commentsFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const fetchPromos = () => (dispatch) => {
+    dispatch(promosLoading(true));
+
+    return fetch(baseUrl + 'promotions')
+        .then(response => response.json())
+        .then(promos => dispatch(addPromos(promos)));
+    // setTimeout(() => { 
+    //     dispatch(addDishes(DISHES));
+    //  }, 2000);
+}
+
+export const promosLoading = ()  => (dispatch) => ({
+    type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
+});
+
+export const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
 });
