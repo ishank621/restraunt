8d0ce0f04a68970fe9904af276534028894a4f9c
Ishank <ishank@classplus.co>
2019-09-12 17:05:44 +0530

    added user login logout and controlled content only on login using sessions

--- NodeJS/restrauntServer/app.js
+++ NodeJS/restrauntServer/app.js
@@ -57,40 +57,21 @@ app.use(
   })
 );
 
+app.use("/", index);
+app.use("/users", users);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic"); // for prompting the user with dialog box
-      err.status = 401;
-      return next(err);
-    }
-
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-
-    var username = auth[0];
-    var password = auth[1];
-
-    if (username === "admin" && password === "password") {
-      req.session.user = "admin";
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+    var err = new Error("You are not authenticated!");
+    err.status = 401;
+    return next(err);
   } else {
-    if (req.session.user === "admin") next();
+    if (req.session.user === "authenticated") next();
     else {
       var err = new Error("You are not authenticated!");
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -100,8 +81,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", index);
-app.use("/users", users);
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
