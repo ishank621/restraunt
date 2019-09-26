0622361bfc35f770d36dd9e26d5258451e9c6405
Ishank <ishank@classplus.co>
2019-09-26 19:42:17 +0530

    added favourites for a particular user and user signed in can do crud on those fav

--- NodeJS/restrauntServer/app.js
+++ NodeJS/restrauntServer/app.js
@@ -14,6 +14,7 @@ var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 var uploadRouter = require("./routes/uploadRouter");
+const favoriteRouter = require("./routes/favouriteRouter");
 var config = require("./config");
 
 const mongoose = require("mongoose");
@@ -97,6 +98,7 @@ app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 app.use("/imageUpload", uploadRouter);
+app.use("/favourites", favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
