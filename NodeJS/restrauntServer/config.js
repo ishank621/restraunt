2b8a3899d9c504a24e64fc818f92b329ca5edaf2
Ishank <ishank@classplus.co>
2019-09-26 17:56:35 +0530

    used facebook app and its token to authenticate using oAuth of fb

--- NodeJS/restrauntServer/config.js
+++ NodeJS/restrauntServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/restraunt"
+  mongoUrl: "mongodb://localhost:27017/restraunt",
+  facebook: {
+    clientId: "603620353500846",
+    clientSecret: "e908a8a282123eda89ec7e47e5b13d38"
+  }
 };
